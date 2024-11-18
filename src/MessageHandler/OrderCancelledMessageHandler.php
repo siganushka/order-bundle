a771b21a1103103be1eaac1c86b6c95ab2fab80c
siganushka <siganushka@gmail.com>
2024-11-18 17:37:38 +0800

    refactor: Increase inventory when order cancalled

--- src/MessageHandler/OrderCancelledMessageHandler.php
+++ src/MessageHandler/OrderCancelledMessageHandler.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Siganushka\OrderBundle\MessageHandler;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Siganushka\OrderBundle\Inventory\OrderInventoryModifierinterface;
 use Siganushka\OrderBundle\Message\OrderCancelledMessage;
 use Siganushka\OrderBundle\Repository\OrderRepository;
 use Symfony\Component\Workflow\WorkflowInterface;
@@ -14,7 +15,8 @@ final class OrderCancelledMessageHandler
     public function __construct(
         private readonly EntityManagerInterface $entityManager,
         private readonly OrderRepository $orderRepository,
-        private readonly WorkflowInterface $orderStateFlow)
+        private readonly WorkflowInterface $orderStateFlow,
+        private readonly OrderInventoryModifierinterface $inventoryModifier)
     {
     }
 
@@ -28,6 +30,7 @@ final class OrderCancelledMessageHandler
         $this->entityManager->beginTransaction();
 
         $this->orderStateFlow->apply($entity, 'cancel');
+        $this->inventoryModifier->modifiy(OrderInventoryModifierinterface::INCREASE, $entity);
 
         $this->entityManager->flush();
         $this->entityManager->commit();
