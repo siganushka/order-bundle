0501f6e2b921088d722fa673cca125c8846b962d
siganushka <siganushka@gmail.com>
2025-12-05 10:06:09 +0800

    refactor: improve OrderItemSubjectType

--- src/Form/Type/OrderItemSubjectType.php
+++ src/Form/Type/OrderItemSubjectType.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 namespace Siganushka\OrderBundle\Form\Type;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\QueryBuilder;
 use Siganushka\Contracts\Doctrine\EnableInterface;
 use Siganushka\GenericBundle\Repository\GenericEntityRepository;
 use Siganushka\OrderBundle\Model\OrderItemSubjectInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\ChoiceList\ChoiceList;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class OrderItemSubjectType extends AbstractType
@@ -23,15 +25,28 @@ class OrderItemSubjectType extends AbstractType
         $classMetadata = $this->entityManager->getMetadataFactory()
             ->getMetadataFor(OrderItemSubjectInterface::class);
 
-        $subjectClass = $classMetadata->getName();
-        $queryBuilder = fn (GenericEntityRepository $er) => is_subclass_of($subjectClass, EnableInterface::class)
-                ? $er->createQueryBuilderWithOrderBy('entity')->andWhere('entity.enabled = :enabled')->setParameter('enabled', true)
-                : $er->createQueryBuilderWithOrderBy('entity');
+        $queryBuilder = function (GenericEntityRepository $er) use ($classMetadata): QueryBuilder {
+            $qb = $er->createQueryBuilderWithOrderBy('entity');
+            if (is_subclass_of($classMetadata->getName(), EnableInterface::class)) {
+                $qb->andWhere('entity.enabled = :enabled')->setParameter('enabled', true);
+            }
+
+            return $qb->setMaxResults(100);
+        };
+
+        $choiceLabel = function (OrderItemSubjectInterface $subject): string {
+            $extra = $subject->getSubjectExtra();
+            if (\is_string($extra)) {
+                return \sprintf('%s【%s】', $subject->getSubjectTitle(), $extra);
+            }
+
+            return $subject->getSubjectTitle();
+        };
 
         $resolver->setDefaults([
             'class' => $classMetadata->getName(),
-            'choice_label' => fn (OrderItemSubjectInterface $subject) => $subject->getSubjectTitle(),
             'query_builder' => $queryBuilder,
+            'choice_label' => ChoiceList::label($this, $choiceLabel),
         ]);
     }
 
