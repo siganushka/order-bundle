acb1d4c1e6a55f0b3cb297fcf803cba4eca6cd84
siganushka <siganushka@gmail.com>
2025-12-05 16:52:46 +0800

    refactor: remove route stateless

--- config/routes.php
+++ config/routes.php
@@ -11,30 +11,25 @@ return static function (RoutingConfigurator $routes): void {
     $routes->add('siganushka_order_getcollection', '/orders')
         ->controller([OrderController::class, 'getCollection'])
         ->methods(['GET'])
-        ->stateless(true)
     ;
 
     $routes->add('siganushka_order_postcollection', '/orders')
         ->controller([OrderController::class, 'postCollection'])
         ->methods(['POST'])
-        ->stateless(true)
     ;
 
     $routes->add('siganushka_order_getitem', '/orders/{number<[a-zA-Z0-9]+>}')
         ->controller([OrderController::class, 'getItem'])
         ->methods(['GET'])
-        ->stateless(true)
     ;
 
     $routes->add('siganushka_order_putitem', '/orders/{number<[a-zA-Z0-9]+>}')
         ->controller([OrderController::class, 'putItem'])
         ->methods(['PUT', 'PATCH'])
-        ->stateless(true)
     ;
 
     $routes->add('siganushka_order_deleteitem', '/orders/{number<[a-zA-Z0-9]+>}')
         ->controller([OrderController::class, 'deleteItem'])
         ->methods(['DELETE'])
-        ->stateless(true)
     ;
 };
